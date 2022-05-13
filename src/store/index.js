016c7b2a5855a0cbdced63d0e56dbc4fe8bae073
Alfred Tuva <muindetuva@gmail.com>
2022-05-13 16:07:36 +0300

    Add request store and complete requests page

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
+import requestModule from './modules/requests/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
+    requests: requestModule,
   },
   state() {
     return {
