d9a76353085988041a0f94c3f07ce231f1151e06
Alfred Tuva <muindetuva@gmail.com>
2022-05-14 13:54:36 +0300

    Add loading and error state

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,27 +1,39 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">email</label>
-        <input type="email" name="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          v-model.trim="password"
-        />
-      </div>
-      <p v-if="!formIsValid">Please enter a valid email and password</p>
+  <div>
+    <base-dialogue
+      :show="!!error"
+      title="An error occured"
+      @close="error = null"
+    >
+      <p>{{ error }}</p>
+    </base-dialogue>
+    <base-dialogue :show="isLoading" fixed title="Authenticating...">
+      <base-spinner></base-spinner>
+    </base-dialogue>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">email</label>
+          <input type="email" name="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            v-model.trim="password"
+          />
+        </div>
+        <p v-if="!formIsValid">Please enter a valid email and password</p>
 
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{
-        submitModeButtonCaption
-      }}</base-button>
-    </form>
-  </base-card>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          submitModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -32,25 +44,33 @@ export default {
       password: '',
       formIsValid: true,
       mode: 'login',
+      isLoading: false,
+      error: null,
     };
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       this.formIsValid = true;
       if (this.email === '' || this.password.length < 6) {
         this.formIsValid = false;
         return;
       }
 
+      this.isLoading = true;
       // send http request
-      if (this.mode === 'login') {
-        // login
-      } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password,
-        });
+      try {
+        if (this.mode === 'login') {
+          // login
+        } else {
+          await this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password,
+          });
+        }
+      } catch (error) {
+        this.error = error.message || 'Something went wrong';
       }
+      this.isLoading = false;
     },
     switchAuthMode() {
       this.mode = this.mode === 'login' ? 'signup' : 'login';
