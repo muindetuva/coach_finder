7cf86dae091bce32f83904ce9a207f346b589e1a
Alfred Tuva <muindetuva@gmail.com>
2022-05-14 13:03:40 +0300

    Add auth page

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,94 @@
+<template>
+  <base-card>
+    <form @submit.prevent="submitForm">
+      <div class="form-control">
+        <label for="email">email</label>
+        <input type="email" name="email" id="email" v-model.trim="email" />
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          v-model.trim="password"
+        />
+      </div>
+      <p v-if="!formIsValid">Please enter a valid email and password</p>
+
+      <base-button>{{ submitButtonCaption }}</base-button>
+      <base-button type="button" mode="flat" @click="switchAuthMode">{{
+        submitModeButtonCaption
+      }}</base-button>
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (this.email === '' || this.password.length < 6) {
+        this.formIsValid = false;
+        return;
+      }
+
+      // send http request
+    },
+    switchAuthMode() {
+      this.mode = this.mode === 'login' ? 'signup' : 'login';
+    },
+  },
+  computed: {
+    submitButtonCaption() {
+      return this.mode === 'login' ? 'Login' : 'Signup';
+    },
+    submitModeButtonCaption() {
+      return this.mode === 'login' ? 'Signup Instead' : 'Login Instead';
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -20,6 +21,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
